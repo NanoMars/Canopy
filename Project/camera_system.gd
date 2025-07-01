1e107c205d38d42c1d5b146a0656a85f9a5ff843
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-07-01 11:58:24 -0700

    sigma scrolling system (SSS)

--- Project/camera_system.gd
+++ Project/camera_system.gd
@@ -1,21 +1,38 @@
 extends Node
 
-@export var camera_marker: PathFollow2D
+@export var path_follow: PathFollow2D
 @export var camera_path: Path2D
-@export var scrolling_thing: Control
+@onready var max_progress: float = camera_path.curve.get_baked_length()
 
-var progress: float:
-	set(value):
-		_progress = value
-		update_marker_position()
-	get:
-		return _progress
+var goal: float = 0.0
+@export var goal_increment: float = 0.1
 
-var _progress: float = 0.0
+var marker_velocity: float = 0.0
+@export var marker_elasticity: float = 0.1
+@export var marker_drag: float = 0.1
+@export var end_buffer: float = 100
+@export var scroll_curve: Curve
 
-func _ready():
-	if camera_marker and camera_path:
-		update_marker_position()
+# how do I do an export 
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			goal += goal_increment
+			goal = clamp(goal, end_buffer, max_progress - end_buffer)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			goal -= goal_increment	
+			goal = clamp(goal, end_buffer, max_progress - end_buffer)
+	elif event is InputEventPanGesture:
+		goal += -event.delta.y * goal_increment
+		goal = clamp(goal, end_buffer, max_progress - end_buffer)
 		
-func update_marker_position():
-	camera_marker.progress_ratio = clamp(progress / 100.0, 0.0, 1.0)
+func _process(delta):
+	var temp_pos = path_follow.progress
+
+
+	marker_velocity += (goal - temp_pos) * marker_elasticity
+	marker_velocity *= marker_drag
+	temp_pos += marker_velocity * delta
+
+	path_follow.progress = temp_pos
