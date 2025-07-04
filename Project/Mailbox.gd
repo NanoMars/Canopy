9b61173357d94584b55879be39b99f7f18b7f118
Armand <174164929+NanoMars@users.noreply.github.com>
2025-07-04 10:42:32 -0700

    Fix mailbox UI

--- Project/Mailbox.gd
+++ Project/Mailbox.gd
@@ -3,9 +3,11 @@ extends TextureButton
 @export var mailbox_UI_scene: PackedScene
 
 func _pressed():
-    if mailbox_UI_scene:
-        var mailbox_ui_instance = mailbox_UI_scene.instantiate()
-        get_parent().add_child(mailbox_ui_instance)
+	if mailbox_UI_scene:
+		var canvas_layer = CanvasLayer.new()
+		var mailbox_ui_instance = mailbox_UI_scene.instantiate()
+		canvas_layer.add_child(mailbox_ui_instance)
+		get_parent().add_child(canvas_layer)
 
 func _ready():
-    pressed.connect(_pressed)
+	pressed.connect(_pressed)
