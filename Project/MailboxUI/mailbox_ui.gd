98d63ec0366f9d60c6f381bdc1d6fd8285d943c3
Armand <174164929+NanoMars@users.noreply.github.com>
2025-07-06 19:51:31 -0700

    make text a variable

--- Project/MailboxUI/mailbox_ui.gd
+++ Project/MailboxUI/mailbox_ui.gd
@@ -1,7 +1,11 @@
 extends Control
 
 @export var letter_buttons: Array[TextureButton] = []
-@export_file("*.txt") var text_path: Array[String]
+@export var text_content: Array[String] = [
+	"CANOPY is a game-focused week full of fun workshops, game creation, and new friends. We’re running three events: Intro to Godot, Workshop Day (3D assets in Blender + Intro to Godot UI), and a Group Game Jam. Attend any of these Canopy events and you’ll earn free-use stipends to spend however you like next week.",
+	"If you are receiving this, it means you are invited to the first event of canopy. But what is such a prestigious event even about?\n\nThis is an in-person event where you will create a platformer in Godot. If you're new to the engine then it will be the perfect introduction, and if you're experienced, it will be a fun opportunity to see how far you can push the idea of a platformer. It's taking place In two locations, we will start at the 55 and second coworking space, and move to the salesforce park for dinner. It will last 8 hours, going from 10am to 6pm. There will be lunch and dinner provided at no cost, and there will be three tokens given out to each person who stays the whole duration.\n\nClimb this branch to get one step closer to the Canopy, we hope to see you there!\n\nsign up: https://forms.hackclub.com/t/u4CsZYUathus",
+	"Free-use stipends let you spend your tokens on anything you like—meals, attractions, travel, or whatever you choose. You earn them as physical wooden tokens by joining Canopy events all week. Each token is worth $5 in stipend cash. At the end of the week, simply turn in your tokens to claim your stipend and spend it however you want."
+]
 
 @export var letter_rtl: RichTextLabel
 
@@ -29,9 +33,7 @@ func _ready() -> void:
 
 
 func _on_letter_button_pressed(index: int) -> void:
-	var file = FileAccess.open(text_path[index], FileAccess.READ)
-	var letter_text = file.get_as_text()
-	file.close()
+	var letter_text = text_content[index]
 	letter_rtl.text = letter_text
 	letter_ui.visible = true
 	mailbox_ui.visible = false
